2d6ae29298f3d310fc61816bfea916485c339f8a
njyangwen <1943073444@qq.com>
2019-06-27 17:32:09 +0800

    ch02 format

--- ch02/HelloBanyuan.java
+++ ch02/HelloBanyuan.java
@@ -1,5 +1,14 @@
-public class HelloBanyuan{
-	public static void main(String[] args){
-		System.out.println("hello banyuan");
-	}
+/*
+ * Copyright (C) 2018-2019 Banyuan All rights reserved
+ */
+/**
+ * 再运行一个java程序
+ * @version 1.0
+ * @author Yang Wen
+ *
+ */
+public class HelloBanyuan {
+    public static void main(String[] args) {
+        System.out.println("hello banyuan");
+    }
 }
