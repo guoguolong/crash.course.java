2107c144430de8cf90f28dc873ba779d7768566d
Allen Guo <guojunlong@tuniu.com>
2019-11-06 21:18:26 +0800

    fix bugs

--- java-ea/javamail/run.sh
+++ java-ea/javamail/run.sh
@@ -1,2 +1,2 @@
-javac -cp libs/activation.jar:libs/javax.mail.jar src/main/java/com/banyuan/javaee/*.java -d target
-java -cp libs/activation.jar:libs/javax.mail.jar:target com.banyuan.javaee.MailClient 
+javac -cp libs/activation.jar:libs/javax.mail.jar src/main/java/club/banyuan/javaee/*.java -d target
+java -cp libs/activation.jar:libs/javax.mail.jar:target club.banyuan.javaee.MailClient 
