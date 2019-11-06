21b6a42195770c32672d6e6a2d35ac3f8c192c0d
Allen Guo <guojunlong@tuniu.com>
2019-11-06 16:50:25 +0800

    update config

--- java-ea/javamail/run-with-mvn.sh
+++ java-ea/javamail/run-with-mvn.sh
@@ -1,1 +1,1 @@
-mvn clean package && java -jar target/mailclient-1.0-jar-with-dependencies.jar
+mvn clean package dependency:copy-dependencies && java -jar target/mailclient-1.0-jar-with-dependencies.jar
