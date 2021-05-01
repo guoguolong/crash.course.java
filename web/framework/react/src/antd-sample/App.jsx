07dea53f716ba667cb352ed6a6915f7782f87619
Allen Guo <elon.guo@gmail.com>
2021-05-01 10:37:18 +0800

    update antd samples

---
+++ web/framework/react/src/antd-sample/App.jsx
@@ -0,0 +1,109 @@
+import React, { useState } from 'react';
+import 'antd/dist/antd.css';
+import { Tree } from 'antd';
+
+const treeData = [
+  {
+    title: 'A-0',
+    key: 'A-0',
+    children: [
+      {
+        title: '0-0-0',
+        key: '0-0-0',
+        children: [
+          {
+            title: '0-0-0-0',
+            key: '0-0-0-0',
+          },
+          {
+            title: '0-0-0-1',
+            key: '0-0-0-1',
+          },
+          {
+            title: '0-0-0-2',
+            key: '0-0-0-2',
+          },
+        ],
+      },
+      {
+        title: '0-0-1',
+        key: '0-0-1',
+        children: [
+          {
+            title: '0-0-1-0',
+            key: '0-0-1-0',
+          },
+          {
+            title: '0-0-1-1',
+            key: '0-0-1-1',
+          },
+          {
+            title: '0-0-1-2',
+            key: '0-0-1-2',
+          },
+        ],
+      },
+      {
+        title: '0-0-2',
+        key: '0-0-2',
+      },
+    ],
+  },
+  {
+    title: '0-1',
+    key: '0-1',
+    children: [
+      {
+        title: '0-1-0-0',
+        key: '0-1-0-0',
+      },
+      {
+        title: '0-1-0-1',
+        key: '0-1-0-1',
+      },
+      {
+        title: '0-1-0-2',
+        key: '0-1-0-2',
+      },
+    ],
+  },
+  {
+    title: '0-2',
+    key: '0-2',
+  },
+];
+
+export default () => {
+  const [expandedKeys, setExpandedKeys] = useState([]);
+  const [checkedKeys, setCheckedKeys] = useState([]);
+  const [selectedKeys, setSelectedKeys] = useState([]);
+  const [autoExpandParent, setAutoExpandParent] = useState(false);
+
+  const onExpand = (expandedKeysValue) => {
+    console.log('onExpand', expandedKeysValue); // if not set autoExpandParent to false, if children expanded, parent can not collapse.
+    setExpandedKeys(expandedKeysValue);
+    setAutoExpandParent(false);
+  };
+
+  const onCheck = (checkedKeysValue) => {
+    console.log('onCheck checkedKeysValue', checkedKeysValue);
+    setCheckedKeys(checkedKeysValue);
+  };
+
+  // const onSelect = (selectedKeysValue, info) => {
+  //   console.log('onSelect', info);
+  //   setSelectedKeys(selectedKeysValue);
+  // };
+
+  return (
+    <Tree
+      checkable
+      onExpand={onExpand}
+      expandedKeys={expandedKeys}
+      autoExpandParent={autoExpandParent}
+      onCheck={onCheck}
+      checkedKeys={checkedKeys}
+      treeData={treeData}
+    />
+  );
+};
